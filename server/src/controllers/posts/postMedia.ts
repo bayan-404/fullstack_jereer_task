2cfa55b275ca9e9282e9f21ea6a8ffbd33354e51
bayan-404 <bayan.jubah@gmail.com>
2022-06-01 16:47:18 +0300

    commented out uploading photo code for debugging

--- server/src/controllers/posts/postMedia.ts
+++ server/src/controllers/posts/postMedia.ts
@@ -4,26 +4,27 @@ import Post from "../../models/posts";
 import { IPost } from "../../types/post";
 
 const postMedia = async (req: any, res: Response): Promise<void> => {
-  try {
-    const file = req?.files?.Image;
-    cloudinary.uploader.upload(file?.tempFilePath, async (err: any, result: any)  => {
-      console.log('Im here')
-      if (err) {
-        return res
-          .status(400)
-          .json({ msg: "error in uploading the file", err });
-      }
+  console.log('cloudinary upload disabled')
+  // try {
+  //   const file = req?.files?.Image;
+  //   cloudinary.uploader.upload(file?.tempFilePath, async (err: any, result: any)  => {
+  //     console.log('Im here')
+  //     if (err) {
+  //       return res
+  //         .status(400)
+  //         .json({ msg: "error in uploading the file", err });
+  //     }
   
-      const mediaPost = { mediaUrl: result.url };
-      const response: IPost = await Post.create(mediaPost);
-      console.log(result.url);
+  //     const mediaPost = { mediaUrl: result.url };
+  //     const response: IPost = await Post.create(mediaPost);
+  //     console.log(result.url);
 
-      return res.status(200).json({ url: result.url });
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Failed to send media.",
-    });
-  }
+  //     return res.status(200).json({ url: result.url });
+  //   });
+  // } catch (error) {
+  //   res.status(500).json({
+  //     message: "Failed to send media.",
+  //   });
+  // }
 };
 export default postMedia;
