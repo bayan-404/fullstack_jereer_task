3d79795dd5ac85b8ba8c68d4b3b90948df056e7d
bayan-404 <bayan.jubah@gmail.com>
2022-06-03 23:02:46 +0300

    add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,42 +1,54 @@
-import express, { Express } from "express";
+import { join } from "path";
 import "dotenv/config";
-import router from "./routes";
+import express, { Express } from "express";
 import { v2 as cloudinary } from "cloudinary";
 import fileUpload from "express-fileupload";
 import mongoose from "mongoose";
+import cors from "cors";
+import router from "./routes";
 
 const app: Express = express();
 const uri: string = `mongodb+srv://${process.env.MONGO_DB_URL}`;
 const PORT: string | number = process.env.PORT || 4000;
-app.use(express.json());
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINERY_API_KEY,
-    api_secret: process.env.CLOUDINERY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINERY_API_KEY,
+  api_secret: process.env.CLOUDINERY_API_SECRET,
 });
 
+const options = {
+  autoIndex: false,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4,
+};
+
+app.use(express.static(join(__dirname, "..", "client", "build")));
+app.use(express.json());
 app.use(
-    fileUpload({
-        useTempFiles: true,
-    })
+  fileUpload({
+    useTempFiles: true,
+  })
 );
 app.use(router);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.get("*", (_req, res) => {
+  res.sendFile(join(__dirname, "..", "client", "build", "index.html"));
+});
 
-const options = {
-    autoIndex: false,
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000,
-    family: 4,
-};
 mongoose
-    .connect(uri, options)
-    .then(() =>
-        app.listen(PORT, () =>
-            console.log(`Server running on http://localhost:${PORT}`)
-        )
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
     )
-    .catch((error) => {
-        throw error;
-    });
+  )
+  .catch((error) => {
+    throw error;
+  });
